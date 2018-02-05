c2a6f33064e716bd84453b16d422b3e05e31f4bf
Novera Lim <lim.novera88@gmail.com>
2018-02-05 22:47:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift('element', 1);
+  array.unshift('element');
   return array;
 }
 
